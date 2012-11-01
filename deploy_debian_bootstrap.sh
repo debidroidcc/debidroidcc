3d3b380d763fa402fe1e1af29a2834403f42f5d7
hflicka <hflicka@gmail.com>
2012-11-01 13:51:58 +0100

    chmod a+x; some additional lines in deploy_debian_boostrap.sh

--- deploy_debian_bootstrap.sh
+++ deploy_debian_bootstrap.sh
@@ -3,12 +3,15 @@
 # this must be executed as superuser
 # debian is then deployed in the /data directory
 
+debootstrap_file_url=https://github.com/downloads/debidroidcc/debidroidcc/debian_bootstrap.tar.gz
 busybox=/system/xbin/busybox
 target_dir=/data
 debian_dir=$target_dir/debian
 
 cd $target_dir
+$busybox wget -O debian_bootstrap.tar.gz $debootstrap_file_url
 $busybox tar -xzf debian_bootstrap.tar.gz
+$busybox rm debian_bootstrap.tar.gz
 
 export PATH=/usr/bin:/usr/sbin:/bin:$PATH
 export HOME=/root
