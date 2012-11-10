ee8a4db6a13f9dea0e108808002c5c66d444b213
Hannes Flicka <hflicka@gmail.com>
2012-11-10 12:48:28 +0100

    gcc cannot be built in its source directory, thus creating new directory for build

--- build-cross-cc.sh
+++ build-cross-cc.sh
@@ -7,27 +7,29 @@ export SRC_ROOT=~
 export BUILD=arm-linux-gnueabihf
 export HOST=arm-linux-gnueabihf
 export TARGET=i686-pc-linux-gnu
-export BUILDDIR=$SRC_ROOT/build
+export SRCDIR=$SRC_ROOT/cross-cc
+export BUILDDIR=$SRCDIR/build
 
 mkdir -p $BUILDDIR
 
-cd $BUILDDIR
+cd $SRCDIR
 wget http://ftp.gnu.org/gnu/binutils/binutils-2.23.tar.gz
 tar -xzf binutils-2.23.tar.gz
-cd binutils-2.23
-./configure --with-included-gettext \
+cd $BUILDDIR
+$SRCDIR/binutils-2.23/configure --with-included-gettext \
 	--target=$TARGET --host=$HOST --build=$BUILD \
 	--prefix=$PREFIX -v
 make
 sudo make install
+rm -rf * .*
 
 # missing here: glibc
 
-cd $BUILDDIR
+cd $SRCDIR
 wget http://ftp.gnu.org/gnu/gcc/gcc-4.6.2/gcc-4.6.2.tar.gz
 tar -xzf gcc-4.6.2.tar.gz
-cd gcc-4.6.2
-./configure --enable-languages=c,c++ \
+cd $BUILDDIR
+$SRCDIR/gcc-4.6.2/configure --enable-languages=c,c++ \
 	--with-included-gettext --enable-shared \
 	--enable-threads=posix \
 	--with-headers=$PREFIX/i686-pc-linux-gnu/include/ \
@@ -35,4 +37,4 @@ cd gcc-4.6.2
 	--prefix=$PREFIX -v
 make
 sudo make install
-
+rm -rf * .*
