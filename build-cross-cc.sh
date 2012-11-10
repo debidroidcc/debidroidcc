e236961b96232325c5bd5ab47748ef25a05ddd2a
Hannes Flicka <hflicka@gmail.com>
2012-11-10 15:22:58 +0100

    added glibc build instructions

--- build-cross-cc.sh
+++ build-cross-cc.sh
@@ -24,6 +24,23 @@ sudo make install
 rm -rf * .*
 
 # missing here: glibc
+# glibc is required in i686-pc-linux-gnu binary form
+# you have to build it on an appropriate machine using:
+#  cd $SRCDIR
+#  wget http://ftp.gnu.org/gnu/glibc/glibc-2.16.0.tar.gz
+#  tar -xzf glibc-2.16.0.tar.gz
+#  cd $BUILDDIR
+#  $SRCDIR/glibc-2.16.0/configure --enable-addons --prefix=$PREFIX/i686-pc-linux-gnu
+#  make
+#  sudo make install
+#  cd $PREFIX
+#  tar -czf $SRC_ROOT/glibc-i686-prefixed.tar.gz i686-pc-linux-gnu
+
+# once compiled, you can put the tarball on your phone and simply unpack:
+#  cd $PREFIX
+#  sudo tar -xzf $SRC_ROOT/glibc-i686-prefixed.tar.gz
+
+# then you can proceed with building gcc as below
 
 cd $SRCDIR
 wget http://ftp.gnu.org/gnu/gcc/gcc-4.6.2/gcc-4.6.2.tar.gz
