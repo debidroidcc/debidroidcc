95dd9871d821b9176ca522a80b8cc0d1868fdad9
Hannes Flicka <hflicka@gmail.com>
2012-12-21 18:22:15 +0100

    Update afterreboot.sh

--- afterreboot.sh
+++ afterreboot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #sudo /etc/init.d/ddclient start # this dyndns client has its flaws
-sudo inadyn # inadyn debian package has no start script in /etc/init.d
+# this fails: sudo inadyn # inadyn debian package has no start script in /etc/init.d
 sudo /etc/init.d/ssh start
 sudo /etc/init.d/dbus start
 sudo /etc/init.d/avahi-daemon start
