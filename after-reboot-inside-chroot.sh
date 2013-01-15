6d8f1aaa2683dc0301de7931d0750f20f6888476
hflicka <hflicka@gmail.com>
2013-01-15 14:14:49 +0100

    commented out inadyn

--- after-reboot-inside-chroot.sh
+++ after-reboot-inside-chroot.sh
@@ -4,7 +4,7 @@
 
 #sudo /etc/init.d/ddclient start # this dyndns client has its flaws
 /etc/init.d/sysklogd start
-nohup inadyn > /dev/null & # inadyn debian package has no start script in /etc/init.d
+# nohup inadyn > /dev/null & # inadyn debian package has no start script in /etc/init.d
 /etc/init.d/ssh start
 /etc/init.d/dbus start
 /etc/init.d/avahi-daemon start
