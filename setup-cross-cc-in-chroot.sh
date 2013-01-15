eae0043457deeb2fd880f73b0bec7cf6df81b316
hflicka <hflicka@gmail.com>
2013-01-15 14:00:58 +0100

    sudo was missing; added checks for built utils

--- setup-cross-cc-in-chroot.sh
+++ setup-cross-cc-in-chroot.sh
@@ -11,9 +11,16 @@ MYDIR=$(dirname "${BASH_SOURCE[0]}")
 MYDIR=$(cd "$MYDIR"; pwd)
 
 # build tools:
-apt-get install -y build-essential
+apt-get install -y build-essential sudo
 # gcc build dependencies:
 apt-get install -y libgmp3-dev libmpfr-dev libmpc-dev
 
 # continue with building the stuff...
 $MYDIR/build-cross-cc.sh
+
+# check if binutils working:
+/usr/local/cross-cc/i686-pc-linux-gnu/bin/ld --version
+
+# check if gcc working:
+/usr/local/cross-cc/i686-pc-linux-gnu/bin/gcc --version
+
