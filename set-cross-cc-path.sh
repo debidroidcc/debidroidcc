39a83444fb7b49a8be7368b8afb7be35455c6235
hflicka <hflicka@gmail.com>
2013-01-16 19:44:36 +0100

    some more paths

--- set-cross-cc-path.sh
+++ set-cross-cc-path.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 export PATH=/usr/local/cross-cc/i686-pc-linux-gnu/bin:$PATH
+export PATH=/usr/local/cross-cc/libexec/gcc/i686-pc-linux-gnu/4.6.2:$PATH
+export LD_LIBRARY_PATH=/usr/local/cross-cc/libexec/gcc/i686-pc-linux-gnu/4.6.2
+export C_INCLUDE_PATH=/usr/local/cross-cc/i686-pc-linux-gnu/include:/usr/local/cross-cc/lib/gcc/i686-pc-linux-gnu/4.6.2/include
+# still doesn't work.. shit
