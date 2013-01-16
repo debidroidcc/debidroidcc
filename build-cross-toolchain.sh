96abff4622e76d038ee5ecf0494391da3cb4c4bd
hflicka <hflicka@gmail.com>
2013-01-16 19:51:55 +0100

    apt-get update required after modifying sources.list

--- build-cross-toolchain.sh
+++ build-cross-toolchain.sh
@@ -15,6 +15,7 @@ unset ARCH
 ARCH=i386
 
 sudo sh -c "echo 'deb-src http://ftp.us.debian.org/debian unstable main contrib non-free' > /etc/apt/sources.list.d/unstable-sources.list"
+sudo apt-get update
 
 apt-get source binutils gcc-4.6
 sudo apt-get build-dep binutils gcc-4.6
