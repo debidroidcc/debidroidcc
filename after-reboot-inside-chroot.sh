9c85a6357dbe0626f72f4e5cde2af5cecb2f15d1
Hannes Flicka <hflicka@gmail.com>
2013-01-20 21:42:31 +0100

    sshd may be already started

--- after-reboot-inside-chroot.sh
+++ after-reboot-inside-chroot.sh
@@ -6,7 +6,7 @@ set -x
 #sudo /etc/init.d/ddclient start # this dyndns client has its flaws
 /etc/init.d/sysklogd start
 # nohup inadyn > /dev/null & # inadyn debian package has no start script in /etc/init.d
-/etc/init.d/ssh start
+/etc/init.d/ssh restart
 /etc/init.d/dbus start
 /etc/init.d/avahi-daemon start
 /etc/init.d/distcc start
