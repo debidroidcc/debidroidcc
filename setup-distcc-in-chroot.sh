c76a30e4a95c2af6698e89d1327d8d3aae4eda7d
Hannes Flicka <hflicka@gmail.com>
2013-01-20 20:49:20 +0100

    added cc1 path

--- setup-distcc-in-chroot.sh
+++ setup-distcc-in-chroot.sh
@@ -7,7 +7,7 @@ set -x
 
 apt-get -y install distcc
 # setup the path to point to the cross-compiler as first directory
-sed -i -e 's|PATH=/usr/local/sbin|PATH=/usr/local/cross-cc/i686-pc-linux-gnu/bin:/usr/local/cross-cc/bin:/usr/local/sbin|g' /etc/init.d/distcc
+sed -i -e 's|PATH=/usr/local/sbin|PATH=/usr/local/cross-cc/libexec/gcc/i686-pc-linux-gnu/4.6.2:/usr/local/cross-cc/i686-pc-linux-gnu/bin:/usr/local/cross-cc/bin:/usr/local/sbin|g' /etc/init.d/distcc
 sed -i -e 's|STARTDISTCC="false"|STARTDISTCC="true"|g' /etc/default/distcc
 sed -i -e 's|ALLOWEDNETS="127.0.0.1"|ALLOWEDNETS="0.0.0.0/0"|g' /etc/default/distcc
 sed -i -e 's|LISTENER="127.0.0.1"|LISTENER="0.0.0.0"|g' /etc/default/distcc
