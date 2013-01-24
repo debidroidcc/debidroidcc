2c404be1d80e5e39041f799b0f747ce8599b704d
debidroidcc <r.kratou@tu-braunschweig.de>
2013-01-24 18:48:38 +0100

    Update deploy-equipped-debian.sh
    unmounting the mounts afterwards and not running the services directly, let the user start them through the gui!

--- deploy-equipped-debian.sh
+++ deploy-equipped-debian.sh
@@ -10,27 +10,28 @@ debian_filename=equipped_debian_chroot.tar.bz2
 equipped_debian_file_url=http://debian-armhf-bootstrap.googlecode.com/files/$debian_filename
 #busybox=/system/xbin/busybox
 if [ -z $busybox ]; then
-	busybox=/data/data/de.tubs.ibr.distcc/app_bin/busybox
+busybox=/data/data/de.tubs.ibr.distcc/app_bin/busybox
 fi
 if [ -z $target_mountpoint ]; then
-	target_mountpoint=/data # this one might be derived from the $target_dir via df or mount
+target_mountpoint=/data # this one might be derived from the $target_dir via df or mount
 fi
 if [ -z $target_dir ]; then
-	target_dir=$target_mountpoint
+target_dir=$target_mountpoint
 fi
 if [ -z $debian_dir ]; then
-	debian_dir=$target_dir/debian
+debian_dir=$target_dir/debian
 fi
 
-echo "Remounting $target_mountpoint"
+echo 'Remounting $target_mountpoint'
 $busybox mount -o remount,exec,dev,suid $target_mountpoint
 
 cd $target_dir
-echo "downloading $debian_filename"
+echo 'Downloading $debian_filename'
 $busybox wget -O $debian_filename $equipped_debian_file_url
-echo "unpacking..."
+echo 'Unpacking...'
 $busybox bzcat $debian_filename | $busybox tar -xf -
 $busybox rm $debian_filename
+#$busybox mv $target_dir/debian $debian_dir 2>/dev/null
 
 export PATH=/usr/bin:/usr/sbin:/bin:$PATH
 export HOME=/root
@@ -50,5 +51,9 @@ $busybox chroot $debian_dir /usr/bin/unzip -o /root/debidroidcc.zip -d /root
 # enable zeroconf as it is not enabled in the tarball
 $busybox chroot $debian_dir /bin/sed -i -e 's|ZEROCONF="false"|ZEROCONF="true"|g' /etc/default/distcc
 
-# finally, start all the services:
-$busybox chroot $debian_dir /bin/bash /root/debidroidcc-master/after-reboot-inside-chroot.sh
+# unmount virtual filesystems
+$busybox chroot $debian_dir /bin/umount /dev/pts
+$busybox chroot $debian_dir /bin/umount /proc
+$busybox chroot $debian_dir /bin/umount /sys
+
+echo 'Installation done!'
