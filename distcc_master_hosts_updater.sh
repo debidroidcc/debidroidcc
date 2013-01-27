c2b3d84a11d4c8a202659356ed1154e92b2dc8a7
debidroidcc <r.kratou@tu-braunschweig.de>
2013-01-27 20:12:20 +0100

    Update distcc_master_hosts_updater.sh
    moar commenting

--- distcc_master_hosts_updater.sh
+++ distcc_master_hosts_updater.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+# deviceStatus E {0,1}
+# STATUS_PLUGGED = 0
+# STATUS_UNPLUGGED = 1
 read deviceStatus
-#read ip and port from remote peer connected to STDIN
+
+# read ip and port from remote peer connected to STDIN
 read ip port <<< `getpeername`
+
+# remove duplicates
 sed -i "/$ip/Id" ~/.distcc/hosts
+
 if [ $deviceStatus == '0' ]; then
 	echo $ip >> ~/.distcc/hosts
 fi
