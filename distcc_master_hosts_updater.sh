5ed3e73696b426ecc47ec8875c3ceffbaa8afe0b
debidroidcc <r.kratou@tu-braunschweig.de>
2013-01-27 19:36:30 +0100

    Update distcc_master_hosts_updater.sh
    commenting

--- distcc_master_hosts_updater.sh
+++ distcc_master_hosts_updater.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 read deviceStatus
+#read ip and port from remote peer connected to STDIN
 read ip port <<< `getpeername`
 sed -i "/$ip/d" ~/.distcc/hosts
 if [ $deviceStatus == '0' ]; then
