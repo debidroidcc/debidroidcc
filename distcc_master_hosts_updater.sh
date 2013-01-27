9fcf7d34307ce3fb6d8d5bbf6addb6900731665e
debidroidcc <r.kratou@tu-braunschweig.de>
2013-01-27 19:48:48 +0100

    Update distcc_master_hosts_updater.sh
    added case insensitive modifier!

--- distcc_master_hosts_updater.sh
+++ distcc_master_hosts_updater.sh
@@ -3,7 +3,7 @@
 read deviceStatus
 #read ip and port from remote peer connected to STDIN
 read ip port <<< `getpeername`
-sed -i "/$ip/d" ~/.distcc/hosts
+sed -i "/$ip/Id" ~/.distcc/hosts
 if [ $deviceStatus == '0' ]; then
 	echo $ip >> ~/.distcc/hosts
 fi
